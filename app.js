6391aeebef02fa7abc898a6ba911b977d3295ca1
Garrik-Liu <garrikliu@Gmail.com>
2018-05-06 00:18:46 -0400

    update

--- app.js
+++ app.js
@@ -1,19 +1,65 @@
-var http = require('http');
+var express = require('express');
+var handlebars = require('express3-handlebars').create({ defaultLayout: 'main' });
 
-http.createServer(function(req, res) {
-    var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
-    switch (path) {
-        case '':
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end('HomePage');
-            break;
-        case '/about':
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end('About');
-            break;
-        default:
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            res.end('Not Found');
-            break;
+var app = express();
+
+var Days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + '/public'));
+
+//process.env 是环境变量
+app.set('port', process.env.PORT || 3000);
+
+app.use(function(req, res, next) {
+    res.locals.showTests = (app.get('env') !== 'production') && (req.query.test === '1');
+    if (res.locals.showTests) {
+        console.log('测试模式启动');
+    } else {
+        console.log('测试模式关闭');
     }
-}).listen(3000);
+    next();
+});
+
+//状态码200为默认状态，不需要指定
+app.get('/', function(req, res) {
+    res.render('home')
+});
+
+app.get('/about', function(req, res) {
+    var randomDay = Days[Math.floor(Math.random() * Days.length)];
+    res.render('about', {
+        day: randomDay,
+        pageTestScript: '/qa/tests-about.js'
+    });
+});
+
+app.get('/contact', function(req, res) {
+    res.render('contact');
+});
+
+app.get('/tours/hood-river', function(req, res) {
+    res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req, res) {
+    res.render('tours/request-group-rate');
+});
+
+// app.use为中间件，目前暂时理解为 前面路由如果都不匹配，则执行下面代码
+app.use(function(req, res) {
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function(req, res) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('App is running!');
+});
